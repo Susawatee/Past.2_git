8d8c18cbb5d527e62f9e19019385d8f3f4ac1810
Susawatee Sama-ae <susawatee.s@ku.th>
2022-06-07 13:05:08 +0700

    Past.2_git

---
+++ test2.py
@@ -0,0 +1,10 @@
+def area(w,l):
+    a = w*l
+    return a
+
+w = float(input("w :"))
+l = float(input("l :"))
+
+area = area(w,l)
+
+print(area)
---
+++ test3.py
@@ -0,0 +1,19 @@
+# def area(w,l):
+#     a = w*l
+#     return a
+
+# x = float(input("w :"))
+# y = float(input("l :"))
+
+# area = area(x,y)
+
+# print(area)
+
+
+def area2(b,h):
+    a = 0.5*b*h
+    return a
+
+b = float(input("b :"))
+h = float(input("h :"))
+print(area2(b,h))
